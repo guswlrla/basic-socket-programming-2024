9a5a026d6aeded65853c7f8bb52280e71cd21cd2
guswlrla <rlaguswl_01@naver.com>
2024-06-12 15:24:28 +0900

    update

--- day02/op_client.c
+++ day02/op_client.c
@@ -5,16 +5,17 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #define BUF_SIZE 1024
+#define RLT_SIZE 4
+#define OPSZ 4
 void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
 	int sock;
-	char message[BUF_SIZE];
-	int str_len;
+	char opmsg[BUF_SIZE];
+	int result, opnd_cnt, i;
 	struct sockaddr_in serv_adr;
-
-	if(argc != 3)	{
+	if(agrc != 3) {
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
@@ -29,30 +30,26 @@ int main(int argc, char *argv[])
 	serv_adr.sin_port = htons(atoi(argv[2]));
 
 	if(connect(sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1)
-		error_handling("connect() error");
+		error_handling("connect() error!");
 	else
-		puts("Connected..........");
+		puts("Connected..............");
 
-	while(1)
+	fputs("Operand count : ", stdout);
+	scanf("%d", &opnd_cnt);
+	opmsg[0] = (char)opnd_cnt;
+
+	for(i = 0; i <opnd_cnt; i++)
 	{
-		fputs("Input message(Q to quit) : ", stdout);
-		fgets(message, BUF_SIZE, stdin);
-		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
-			break;
-
-		str_len = write(sock, message, strlen(message));
-
-		recv_len = 0;
-		while(recv_len < str_len)
-		{
-			recv_cnt = read(sock, &message[recv_len], BUF_SIZE-1);
-			if(recv_cnt == -1)
-				error_handling("read() error");
-			recv_len += recv_cnt;
-		}
-		message[recv_len] = 0;
-		printf("Message from server : %s", message);
+		printf("Operand %d : ", i + 1);
+		scanf("%d", (int*)&opmsg[i*OPSZ+1]);
 	}
+	fgetc(stdin);
+	fputs("Operator : ", stdout);
+	scanf("%c", &opmsg[opnd_cnt*OPSZ+1]);
+	write(sock, opmsg, opnd_cnt*OPSZ+2);
+	read(sock, &result, RLT_SIZE);
+
+	printf("Operation result : %d \n", result);
 	close(sock);
 	return 0;
 }
